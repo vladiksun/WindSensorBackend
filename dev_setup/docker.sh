bd1b28fe01d183c7fcabbad2de8ff5ffa9e18833
vlad <vladiksun85@gmail.com>
2025-09-03 13:56:28 +0300

    docker settings

--- dev_setup/docker.sh
+++ dev_setup/docker.sh
@@ -1,1 +1,13 @@
+docker images
+
+docker rmi windsensorbackend
+
+docker load -i jib-image.tar
+
 docker run --rm -it -p 443:443 windsensorbackend:latest
+
+docker run --rm -it \
+  -p 443:443 \
+  -v /home/keystore.p12:/home/keystore.p12:ro \
+  -e KEY_STORE_PATH=file:/home/keystore.p12 \
+windsensorbackend:latest
