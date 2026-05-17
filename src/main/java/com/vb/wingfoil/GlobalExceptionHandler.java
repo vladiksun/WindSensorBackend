b7e33fb2cee6cc6bde227b15617f69ca5d7c9c4a
vladiksun <vladiksun85@gmail.com>
2026-05-17 19:34:00 +0300

    refactor: move global error handling to dedicated ExceptionHandler bean

---
+++ src/main/java/com/vb/wingfoil/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.vb.wingfoil;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.hateoas.JsonError;
+import io.micronaut.http.hateoas.Link;
+import io.micronaut.http.server.exceptions.ExceptionHandler;
+import jakarta.inject.Singleton;
+
+@Singleton
+@Requires(classes = ExceptionHandler.class)
+public class GlobalExceptionHandler implements ExceptionHandler<Throwable, HttpResponse<JsonError>> {
+
+    @Override
+    public HttpResponse<JsonError> handle(HttpRequest request, Throwable exception) {
+        var error = new JsonError(exception.getMessage())
+                .link(Link.SELF, Link.of(request.getUri()));
+
+        return HttpResponse.<JsonError>serverError()
+                .body(error);
+    }
+}
--- src/main/java/com/vb/wingfoil/WindSenorController.java
+++ src/main/java/com/vb/wingfoil/WindSenorController.java
@@ -1,11 +1,6 @@
 package com.vb.wingfoil;
 
-import io.micronaut.http.HttpRequest;
-import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.*;
-import io.micronaut.http.annotation.Error;
-import io.micronaut.http.hateoas.JsonError;
-import io.micronaut.http.hateoas.Link;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 import io.vavr.control.Option;
@@ -41,15 +36,6 @@ public class WindSenorController {
     @ExecuteOn(TaskExecutors.VIRTUAL)
     public List<SpotDataDTO> getSpotsDataDahab(@QueryValue(defaultValue = "false") boolean isDebug) {
         return proxyService.requestSpotsDataForDahab(isDebug).get();
-    }
-
-    @Error(global = true)
-    public HttpResponse<JsonError> error(HttpRequest<?> request, Throwable e) {
-        var error = new JsonError(e.getMessage())
-                .link(Link.SELF, Link.of(request.getUri()));
-
-        return HttpResponse.<JsonError>serverError()
-                .body(error);
     }
 
 }
